6aa07c0a70efed3fec20094c48189ae1a5148f47
hackathon7 <hackathon7@rohde-schwarz.com>
2023-11-19 00:13:56 +0100

    added snippets of logfile and tab strutcture

--- streamlit/Home.py
+++ streamlit/Home.py
@@ -5,8 +5,16 @@ import tempfile
 from langchain.document_loaders import DirectoryLoader, TextLoader
 from utils.chat_with_log import handle_chat, request_summary_from_analysis
 from utils.create_db import create_database
+import re
 
+st.set_page_config(page_title="Chat", layout='wide')
+st.header('Dora the Log-Explorer')
+desc = """Start questioning Dora about the specified log file."""
+st.markdown(desc)
 
+col1, col2 = st.columns(2)
+st.subheader('Ask questions about your log file')
+tab1, tab2 = st.tabs(['Chat', 'Log File Snippets']) 
 
 # Corrected: st.set_page_config should only be called once at the beginning of the script
 
@@ -75,6 +83,40 @@ def display_message(message, is_user=True):
     """
     st.markdown(html, unsafe_allow_html=True)
 
+def extract_lines_with_dates(text):
+    # Regex pattern to match date format "Month Abbreviation Day Time" for any month, and capture the entire line
+    pattern = r"(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s+\d+\s+\d{2}:\d{2}:\d{2}.*"
+    matches = re.finditer(pattern, text)
+
+    # Extract and return the matched lines
+    return [match.group(0) for match in matches]  # group(0) contains the entire matched string
+
+def find_line_in_file(file_path, search_text):
+    """
+    Search for a line in a file that contains the given text.
+
+    Args:
+    file_path (str): Path to the file.
+    search_text (str): Text to search for in the file.
+
+    Returns:
+    int: The line number of the first line containing the search text, or -1 if not found.
+    """
+    with open(file_path, 'r') as file:
+        for line_number, line in enumerate(file, 1):
+            if search_text in line:
+                return line_number
+    return -1
+
+def read_file_lines(file_path, start_line, end_line):
+    """Reads specified line range from a file."""
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+    # Adjust line numbers to be zero-indexed
+    start_index = max(start_line - 1, 0)
+    end_index = min(end_line, len(lines))
+    return lines[start_index:end_index]
+
 
 # Function to handle sending messages
 def send_message():
@@ -92,25 +134,49 @@ def send_message():
     # Reset the input box
     st.session_state['user_input'] = ""
 
+# Function to handle sending messages
+def send_message():
+    user_input = st.session_state.get('user_input', '')
+    if user_input:  # Check if the input is not empty
+        add_to_conversation(user_input, is_user=True)
+        
+        # Get chatbot response and add to conversation
+        response = chatbot_response(user_input)
+        add_to_conversation(response, is_user=False)
+
+        with open("summary.txt", 'r') as file:
+            summary_text=file.read()
+        st.session_state['summary'] = summary_text
+
+        if saved_file_path is not None:
+            # Extracting and printing the relevant lines
+            extracted_lines = extract_lines_with_dates(response)
+            for extracted_line in extracted_lines:
+                line_number = find_line_in_file(saved_file_path, extracted_line)
+
+                # Process file and display lines
+                if line_number != -1:
+                    if line_number <= 10:
+                        start_line, stop_line = 0, line_number
+                    else:
+                        start_line, stop_line = line_number - 10, line_number
+                    selected_lines = read_file_lines(saved_file_path, start_line, stop_line)
+                    with tab2:
+                        st.text_area("Displaying lines from {} to {}:".format(start_line, stop_line), ''.join(selected_lines), height=360)
+
 # New function to add messages to conversation
 def add_to_conversation(message, is_user=True):
     st.session_state['conversation'].append(message)
 
-
-st.header('Dora the Log-Explorer')
-
-desc = """
-Start questioning Dora about the specified log file.
-"""
-st.markdown(desc)
-
-col1, col2 = st.columns(2)
+saved_file_path = None
 
 
-# In your Streamlit app
-with col1:
-    if not st.session_state.get('file_processed'):
-        uploaded_file = st.file_uploader("Upload a log file", type=["txt", "out"], key="log_file_uploader")
+with tab1:
+    # In your Streamlit app
+    with col1:
+        st.subheader('File Upload')
+        if not st.session_state.get('file_processed'):
+            uploaded_file = st.file_uploader("Upload a log file", type=["txt", "out"], key="log_file_uploader")
 
         if uploaded_file is not None:
             st.write("Uploading File")
@@ -132,31 +198,31 @@ with col1:
         else:
             st.write("Please upload a text file.")
 
-with col2:
-    # Section for summary at the top of col2
-    st.subheader("Log Summary")
-    if 'summary' in st.session_state and st.session_state['summary']:
-        st.text_area("Summary Text", value=st.session_state['summary'], height=300)
-        with open("summary.txt", 'r') as file:
-            summary_text=file.read()
-            st.session_state['summary'] = summary_text
-    else:
-        st.write("The summary will appear here after you upload and process a log file.")
+    with col2:
+        # Section for summary at the top of col2
+        st.subheader("Log Summary")
+        if 'summary' in st.session_state and st.session_state['summary']:
+            st.text_area("Summary Text", value=st.session_state['summary'], height=300)
+            with open("summary.txt", 'r') as file:
+                summary_text=file.read()
+                st.session_state['summary'] = summary_text
+        else:
+            st.write("The summary will appear here after you upload and process a log file.")
 
-# Now comes the conversation part, below the summary in col2
-st.subheader("Conversation")
-if 'conversation' not in st.session_state:
-    st.session_state['conversation'] = []
+    # Now comes the conversation part, below the summary in col2
+    st.subheader("Conversation")
+    if 'conversation' not in st.session_state:
+        st.session_state['conversation'] = []
 
-# Display existing conversation
-for message in st.session_state['conversation']:
-    is_user = not st.session_state['conversation'].index(message) % 2 == 0
-    display_message(message, is_user)
+    # Display existing conversation
+    for message in st.session_state['conversation']:
+        is_user = not st.session_state['conversation'].index(message) % 2 == 0
+        display_message(message, is_user)
 
-# Text input for user message with a callback
-st.text_input("Your message", key="user_input", on_change=send_message, value="")
+    # Text input for user message with a callback
+    st.text_input("Your message", key="user_input", on_change=send_message, value="")
 
-# Button to send the message
-if st.button('Send'):
-    st.write("Thinking ... ")
-    send_message()
+    # Button to send the message
+    if st.button('Send'):
+        st.write("Thinking ... ")
+        send_message()
